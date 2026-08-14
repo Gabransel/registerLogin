e7a83012674a09f3163cdfbf7f9c54c6ea2a8b2e
Gabriel <gabriel.anselmo.98@gmail.com>
2026-08-14 15:15:47 -0300

    Project Created

---
+++ src/test/java/Gabransel/RegisterLogin/RegisterLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package Gabransel.RegisterLogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
